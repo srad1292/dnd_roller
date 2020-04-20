6aa399dd2fcb14343f266ffb9f9359546c5f8bb9
Sam Radford <smr1292@yahoo.com>
2020-04-19 22:32:27 -0400

    logic to add and display a new row to represent a dice in a roll

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let die = 4;
-let rolls = 1;
-let modifier = 0;
-let typeOfRoll = 'regular';
+this.die = 4;
+this.rolls = 1;
+this.modifier = 0;
+this.typeOfRoll = 'regular';
+this.counter = 0;
 
 function rollDice(formElement) {
+    console.log("dice: ", this.die);
+    console.log({formElement});
     this.getFormValues(formElement);
     const rolls = [];
     rolls.push(this.performRoll());
@@ -78,3 +81,99 @@ function sortRolls(sums) {
     });
 }
 
+
+function addDiceRow(){
+    try{
+        this.addContainer();
+        this.addTypeOfDiceInput();
+        this.addNumberOfRollsInput();
+        this.addModifierInput();
+        this.addRemoveButton();
+    } catch(error){
+        console.log("Error adding row");
+        console.log(error);
+    }
+}
+
+
+function addContainer() {
+    console.log("counter A: ", this.counter);
+    this.counter++;
+    console.log("counter: ", this.counter);
+    let newContainer = document.createElement('div');
+    newContainer.classList.add('flex-form');
+    newContainer.setAttribute('id', `dice-row-${this.counter}`);
+    document.getElementById('additional-dice-container').appendChild(newContainer);
+}
+
+function addTypeOfDiceInput() {
+    const container = this.addNewElementContainer();
+
+    let typeOfDieLabel = document.createElement('label');
+    typeOfDieLabel.htmlFor = `typeOfDie${this.counter}`;
+    typeOfDieLabel.innerText = "Type of Die:";
+    
+    let typeOfDieSelect = document.createElement('select');
+    typeOfDieSelect.setAttribute('id', `typeOfDie${this.counter}`);
+
+    [4, 6, 8, 10, 12, 20, 100].forEach(sides => {
+        let diceOption = document.createElement("option");
+        diceOption.text = `D${sides}`;
+        diceOption.value = `${sides}`;
+        typeOfDieSelect.appendChild(diceOption);
+    });
+    
+    container.appendChild(typeOfDieLabel);
+    container.appendChild(typeOfDieSelect);
+}
+
+function addNumberOfRollsInput() {
+    const container = this.addNewElementContainer();
+
+    let numberOfRollsLabel = document.createElement('label');
+    numberOfRollsLabel.htmlFor = `numberOfRolls${this.counter}`;
+    numberOfRollsLabel.innerText = "Number of Rolls:";
+
+    let numberOfRollsInput = document.createElement('input');
+    numberOfRollsInput.setAttribute('type', 'number');
+    numberOfRollsInput.setAttribute('id', `numberOfRolls${this.counter}`);
+    numberOfRollsInput.setAttribute('required', "required");
+
+
+    container.appendChild(numberOfRollsLabel);
+    container.appendChild(numberOfRollsInput);
+}
+
+function addModifierInput() { 
+    const container = this.addNewElementContainer();
+
+    let modifierLabel = document.createElement('label');
+    modifierLabel.htmlFor = `modifier${this.counter}`;
+    modifierLabel.innerText = "Modifier:";
+
+    let modifierInput = document.createElement('input');
+    modifierInput.setAttribute('type', 'number');
+    modifierInput.setAttribute('id', `modifier${this.counter}`);
+    modifierInput.setAttribute('required', "required");
+
+
+    container.appendChild(modifierLabel);
+    container.appendChild(modifierInput);
+}
+
+function addRemoveButton() { 
+    const container = this.addNewElementContainer();
+    container.classList.add('remove-button-container');
+    let removeButton = document.createElement('button');
+    removeButton.setAttribute('type', 'button');
+    removeButton.innerText ="Remove";
+
+    container.appendChild(removeButton);
+}
+
+function addNewElementContainer() {
+    let newContainer = document.createElement('div');
+    newContainer.classList.add('form-element');
+    document.getElementById(`dice-row-${this.counter}`).appendChild(newContainer);
+    return newContainer;
+}
